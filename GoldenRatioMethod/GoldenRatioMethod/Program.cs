c765e6e9c7a6324a72e2b57fb0bc4c88dc66a7ee
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2023-12-12 20:40:09 +0700

    file fix

---
+++ GoldenRatioMethod/GoldenRatioMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using org.mariuszgromada.math.mxparser.mathcollection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoldenRatioMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Application.Run(new MainForm());
+
+      
+      double x = 0.729876543242353452361 / Math.Pow(10,4);
+      Console.WriteLine("неокругленный: " + x);
+
+      Console.WriteLine("округленный round: " + Math.Round(x, 5));
+
+      double a = Math.Truncate(x * Math.Pow(10, 5));
+      Console.WriteLine("округленный: " + a / Math.Pow(10, 5));
+
+      double b = Math.Truncate(x * Math.Pow(10, 5));
+      Console.WriteLine(b);
+      Console.ReadKey();
+    }
+  }
+}
